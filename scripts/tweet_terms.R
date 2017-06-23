dcbd1ebc851178c8509813bd16e4250ad231cccb
Mara Averick <maraaverick@gmail.com>
2017-06-23 13:21:18 -0400

    Quick list of possible descriptors and enhancers

---
+++ scripts/tweet_terms.R
@@ -0,0 +1,5 @@
+## Possibly notable descriptive terms for twitter analysis
+descriptors <- c("icymi", "how to", "step-by-step", "walk-through", "code-through", "tutorial", "intro")
+
+## Same for qualitative/feeling words (possibly add emoji)
+enhancers <- c("great", "nice", "awesome", "fab")
